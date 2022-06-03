750c0ddd5f71290d39d1c360236f38c890910df8
Diogo de Souza <48259656+souzadediogo@users.noreply.github.com>
2022-06-03 15:51:00 -0300

    Delete dist directory

--- dist/modules/meliAuth/repositories/IMeliAuthCodeRepository.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-// import { SalesChannel } from "../entities/SalesChannels";
-// import { Seller } from "../entities/Seller";
-;
-;
--- dist/modules/offers/interfaces/IOffersRepository.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-// import { IItems } from "@modules/offers/useCases/createOffer/CreateOfferUseCase"
-;
