c626108926d0b2943b45b895a62b3551313c132e
Diogo de Souza <hello@diogodesouza.com>
2022-07-04 20:52:15 -0300

    Correcting gdd

--- dist/services/meliServices/getDailyData/index.js
+++ dist/services/meliServices/getDailyData/index.js
@@ -25,44 +25,46 @@ async function getDailyData() {
   console.log(`ChannelIDs`, channelIds); // Get all offers MLBs from each salesChannel
 
   async function runScript(salesChannel) {
-    return new Promise((resolve, reject) => {
-      const myMLBs = [];
-      let currentChannel = salesChannel; //channelIds[salesChannel];
-
-      let script = offerServices.getAllOffersBySalesChannelID(currentChannel).then(response => {
-        for (let offer in response.data) {
-          myMLBs.push(response.data[offer].offerID);
-        }
-
-        console.log(`Starting to fetch offers in Meli from channel ${currentChannel}`);
-        meliServices.multiGetBatchOfOffers(myMLBs).then(res2 => {
-          // console.log('res2', `${res2.length}`)
-          console.log(`Finished fetching offers in Meli from channel ${currentChannel}`);
-          console.log(`Current Channel:`, currentChannel); //Mapear Array<MeliOffer[]> para array de createdailydata
-          // console.log(`array in res2:`, res2)
-
-          meliServices.mapMeliOfferArrayToDailyDataInterface(currentChannel, res2).then(res3 => {
-            console.log(`Mapping offers from channel ${currentChannel}`); // console.log(`res3 in savebatch has ${res3.length} offers from channel ${currentChannel}`)
-
-            meliServices.saveBatchDailyData(res3).then(res4 => {
-              console.log(`Done saving channel ${currentChannel}`);
-              resolve({
-                message: `Success retrieving daily data from ${currentChannel}`
-              }); // return 
+    try {
+      return new Promise((resolve, reject) => {
+        const myMLBs = [];
+        let currentChannel = salesChannel; //channelIds[salesChannel];
+  
+        let script = offerServices.getAllOffersBySalesChannelID(currentChannel).then(response => {
+          for (let offer in response.data) {
+            myMLBs.push(response.data[offer].offerID);
+          }
+  
+          console.log(`Starting to fetch offers in Meli from channel ${currentChannel}`);
+          meliServices.multiGetBatchOfOffers(myMLBs).then(res2 => {
+            // console.log('res2', `${res2.length}`)
+            console.log(`Finished fetching offers in Meli from channel ${currentChannel}`);
+            console.log(`Current Channel:`, currentChannel); //Mapear Array<MeliOffer[]> para array de createdailydata
+            // console.log(`array in res2:`, res2)
+  
+            meliServices.mapMeliOfferArrayToDailyDataInterface(currentChannel, res2).then(res3 => {
+              console.log(`Mapping offers from channel ${currentChannel}`); // console.log(`res3 in savebatch has ${res3.length} offers from channel ${currentChannel}`)
+  
+              meliServices.saveBatchDailyData(res3).then(res4 => {
+                console.log(`Done saving channel ${currentChannel}`);
+                resolve({
+                  message: `Success retrieving daily data from ${currentChannel}`
+                }); // return 
+              }, err => {
+                console.log(err);
+              });
             }, err => {
-              console.log(err);
+              console.log(`erro em mapMeliOfferArrayToDailyDataInterface`, err);
             });
           }, err => {
-            console.log(`erro em mapMeliOfferArrayToDailyDataInterface`, err);
+            console.log(`erro em multiGetBatchOfOffers`, err);
           });
         }, err => {
-          console.log(`erro em multiGetBatchOfOffers`, err);
+          console.log("erro aqui no fim", err);
+          reject("Failure");
         });
-      }, err => {
-        console.log("erro aqui no fim", err);
-        reject("Failure");
-      });
-    }); //End of promisse
+      }); //End of promisse  
+    }catch(e){console.log(E)}
   } // let numOfSalesChannels = channelIds.length;
   // let currentSalesChannel = 0;
   // while(currentSalesChannel<channelIds.length){
